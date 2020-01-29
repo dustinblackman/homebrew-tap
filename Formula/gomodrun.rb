f539726a3bc2730e3b47b4c36c86780d71e80d23
goreleaserbot <goreleaser@carlosbecker.com>
2020-01-29 17:15:26 -0500

    Brew formula update for gomodrun version v0.1.3

--- Formula/gomodrun.rb
+++ Formula/gomodrun.rb
@@ -2,24 +2,24 @@
 class Gomodrun < Formula
   desc "The forgotten go tool that executes and caches binaries included in go.mod files"
   homepage "https://github.com/dustinblackman/gomodrun"
-  version "0.1.1"
+  version "0.1.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dustinblackman/gomodrun/releases/download/v0.1.1/gomodrun_0.1.1_darwin_amd64.tar.gz"
-    sha256 "a7318068411569c5526dc9020a3b5a458cdb49a9577bc00dae48b7c51edc3f75"
+    url "https://github.com/dustinblackman/gomodrun/releases/download/v0.1.3/gomodrun_0.1.3_darwin_amd64.tar.gz"
+    sha256 "bdd41089659a50b314707135ca977204e0e679f3a2c874e565eb77f6c21b129f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/dustinblackman/gomodrun/releases/download/v0.1.1/gomodrun_0.1.1_linux_amd64.tar.gz"
-      sha256 "6468152e545c5f7f277c59cc3d279812f3868a6e9f9da916e9a0475f4af5f4bb"
+      url "https://github.com/dustinblackman/gomodrun/releases/download/v0.1.3/gomodrun_0.1.3_linux_amd64.tar.gz"
+      sha256 "e987c548969e0fe97aabdd36611ec391b60a4c2ff36ee69c06897fcef3959905"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/dustinblackman/gomodrun/releases/download/v0.1.1/gomodrun_0.1.1_linux_arm64.tar.gz"
-        sha256 "a5f1bc18d0ac13a9902e2a44cc2ec0b2e7bc2fde8083a4e283c60a8a4d699ab7"
+        url "https://github.com/dustinblackman/gomodrun/releases/download/v0.1.3/gomodrun_0.1.3_linux_arm64.tar.gz"
+        sha256 "ca2714060ec3f46b57810fb43f9feb5783e4976aeec308dc39643a53db035a23"
       else
-        url "https://github.com/dustinblackman/gomodrun/releases/download/v0.1.1/gomodrun_0.1.1_linux_armv6.tar.gz"
-        sha256 "1ceef43829c6a343614d5f241f54ee0e4684aecba7e795ce6f9a2b607ff8cba4"
+        url "https://github.com/dustinblackman/gomodrun/releases/download/v0.1.3/gomodrun_0.1.3_linux_armv6.tar.gz"
+        sha256 "215bf5eaad1d50c56ece17108e9503f0d6b868857d3fc2f6f72ef7f7d875bf3e"
       end
     end
   end
