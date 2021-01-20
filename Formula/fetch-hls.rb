9a3b6b5e87cb269770ed12c32c8bd342241011b5
goreleaserbot <goreleaser@carlosbecker.com>
2021-01-20 09:21:36 -0500

    Brew formula update for fetch-hls version v0.1.5

--- Formula/fetch-hls.rb
+++ Formula/fetch-hls.rb
@@ -5,24 +5,24 @@
 class FetchHls < Formula
   desc "A quick and lazy solution to proxy HLS streams to external players (Chromecast, VLC)."
   homepage "https://github.com/dustinblackman/fetch-hls"
-  version "0.1.4"
+  version "0.1.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dustinblackman/fetch-hls/releases/download/v0.1.4/fetch-hls_0.1.4_darwin_amd64.tar.gz"
-    sha256 "8394f5c0ca0f763ff6d9ef4f29b743bd828aa21174bcddf217fc2159f515b67f"
+    url "https://github.com/dustinblackman/fetch-hls/releases/download/v0.1.5/fetch-hls_0.1.5_darwin_amd64.tar.gz"
+    sha256 "e207ff616f08c62aaaa8c216114642af873a4715069654397baa6bc065be4e22"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/dustinblackman/fetch-hls/releases/download/v0.1.4/fetch-hls_0.1.4_linux_amd64.tar.gz"
-    sha256 "e2cb86595b92908fb39a0c8f512e240b289f7263dec81c45d2b14a7e9e4be61a"
+    url "https://github.com/dustinblackman/fetch-hls/releases/download/v0.1.5/fetch-hls_0.1.5_linux_amd64.tar.gz"
+    sha256 "244a7b17f2a717f54c03c3b1791ea3e9dccf9d36fb7c448cc940f38da33eb2b0"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/dustinblackman/fetch-hls/releases/download/v0.1.4/fetch-hls_0.1.4_linux_armv6.tar.gz"
-    sha256 "4c64554277d81e6770e3245db303ff9a8911fe79545489a33cb9b5dd921220a9"
+    url "https://github.com/dustinblackman/fetch-hls/releases/download/v0.1.5/fetch-hls_0.1.5_linux_armv6.tar.gz"
+    sha256 "0db56f98397df2cf6dc68a49974bf01971f0d25bfc4a6dd98431af59709f53c1"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/dustinblackman/fetch-hls/releases/download/v0.1.4/fetch-hls_0.1.4_linux_arm64.tar.gz"
-    sha256 "c8bf635539116ca991a51139b046a7c866def9fd5824558857820938ca35a2ef"
+    url "https://github.com/dustinblackman/fetch-hls/releases/download/v0.1.5/fetch-hls_0.1.5_linux_arm64.tar.gz"
+    sha256 "ba738a600331e2a177770cce56b9befd8ca2b0f2387772a94d42e94dd5b0899f"
   end
 
   def install
